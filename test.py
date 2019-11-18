9f3bab5e4dcd5b3beb09bc30ee9936fd1349ca83
Joel Anderton <anderton.joel@gmail.com>
2019-11-17 20:05:06 -0500

    Add reliability to the randomization process.

--- test.py
+++ test.py
@@ -17,9 +17,16 @@ random.shuffle(studyID_list)
 len_list = len(studyID_list)
 rater2_percent = 40 / 100
 rater3_percent = 40 / 100
+reliablity_percent = 10 / 100
 
-rater2_end_list = int(len_list * rater2_percent)
-rater3_end_list = int(len_list * rater3_percent)
+reliablity_end_list = int(len_list * reliablity_percent)
+rater2_end_list = int(round(len_list * rater2_percent))
+rater3_end_list = int(round(len_list * rater3_percent))
+
+random.shuffle(studyID_list)
+reliability_list = []
+for i in studyID_list[:reliablity_end_list]:
+    reliability_list.append(i)
 
 rater2 = []
 for i in studyID_list[:rater2_end_list]:
@@ -29,7 +36,14 @@ rater3 = []
 for i in studyID_list[:rater3_end_list]:
     rater3.append(studyID_list.pop())
 
-rater1 = studyID_list
+
+rater1 = studyID_list + reliability_list
+random.shuffle(rater1)
+rater2 = rater2 + reliability_list
+random.shuffle(rater2)
+rater3 = rater3 + reliability_list
+random.shuffle(rater3)
+
 print(rater3)
 print(rater2)
 print(rater1)
